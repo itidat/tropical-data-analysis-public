10ed740b39a8a5fa135057ac9a3c86612f2c74b5
abakhtiari <35935699+abakhtiari@users.noreply.github.com>
2025-09-12 08:33:51 -0700

    2.0 updates

--- source_code/writeAnalysisResults.R
+++ source_code/writeAnalysisResults.R
@@ -1,10 +1,5 @@
 ## Tropical Data standard analyses - generate and export file with analysis results by EU
-## exported results will match the District Report output downloadable here : https://www.tropicaldata.org/downloads
-## v. 14/08/20 
-
-# remove temporary objects 
-rm(tfprev_cluster, ttprev_cluster)
-
+## v. 9 September 2025 
 
 # rename tt1 and tt2 result columns to match field names in District Report output 
 tt1_results <- tt1_eu_CIs %>% 
@@ -16,7 +11,8 @@ tt2_results <- tt2_eu_CIs %>%
 
 # merge all results into one dataframe  
 analysis_result <- eu_cross %>% 
-  select(admin0_name,	admin1_name, admin2_name,	admin3_name, eu, project_id, survey_type) %>% 
+  select(admin0_name,	admin1_name, admin2_name,	admin3_name, eu_id, project_id, survey_type) %>% 
+  rename(eu = eu_id) %>% 
   right_join(eu_list) %>% 
   left_join(select(counts_eu, eu, date_completed, cluster_ct, hh_ct, child_enum, child_ex), by = "eu") %>% 
   left_join(select(tfprev_eu_CIs, eu, tf_adj, tf_95_low, tf_95_upp), by = "eu") %>% 
@@ -28,12 +24,10 @@ analysis_result <- eu_cross %>%
   arrange(eu)
 
 # get country name for file name 
-# NOTE: if your project is at the region level, change line 32 to -> country <- as.character(eu_cross[1,2])
-country <- as.character(eu_cross[1,1])
+# NOTE: if your project is at the region level, change line 32 to -> country <- as.character(eu_cross[1,4])
+country <- as.character(eu_cross[1,3])
 
 # write results to file in your working directory 
-# the results in this file can be pasted into the District Report template downloadable here : https://www.tropicaldata.org/downloads
-# the District Report template includes a data dictionary with definitions for each variable of the results output 
 write.csv(analysis_result, paste("analysis_result_",country,"_",currentDate,".csv", sep=""), na = "", row.names = FALSE)
 
 ### END ###
